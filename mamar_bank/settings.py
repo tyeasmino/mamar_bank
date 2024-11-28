274a4ab73ac969bef4bbc14102cf2f9dec0d810c
tyeasmino <tyeasmino@gmail.com>
2024-11-28 10:57:27 +0600

    FO: CSRF Token Updated

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -33,7 +33,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'mamar-bank-c143.onrender.com']
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://mamar-bank-c143.onrender.com', 'https://*.127.0.0.1']
 
 
 # Application definition
