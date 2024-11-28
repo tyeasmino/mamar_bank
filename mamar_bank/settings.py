34b6b8df0cbcf9eb6a520e0c70b1be1660ad931f
Tamima Yeasmin <tyeasmino@gmail.com>
2024-11-28 09:21:46 +0600

    allowed_host updated

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', 'mamar-bank-c143.onrender.com']
 
 
 # Application definition
